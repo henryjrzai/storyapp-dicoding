38f48c75c73aef1539dfab850d33ca9512c112a6
Henry Zai <hjuniorzai@gmail.com>
2023-10-18 00:27:39 +0700

    make intent gallery

--- app/src/main/java/com/hjz/storyapp/addStories/AddStoriesActivity.kt
+++ app/src/main/java/com/hjz/storyapp/addStories/AddStoriesActivity.kt
@@ -1,15 +1,44 @@
 package com.hjz.storyapp.addStories
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.hjz.storyapp.R
+import android.util.Log
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import com.hjz.storyapp.databinding.ActivityAddStoriesBinding
 
 class AddStoriesActivity : AppCompatActivity() {
     private lateinit var binding : ActivityAddStoriesBinding
+    private var currentImageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddStoriesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnGallery.setOnClickListener {
+            startGallery()
+        }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.imgStories.setImageURI(it)
+        }
     }
 }
